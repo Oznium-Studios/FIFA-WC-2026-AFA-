ca5d2f7d4190f68fbaef7a41d8c9726e57daf42c
Mashrafi <mashrafi01cppjs@gmail.com>
2026-06-12 17:03:58 +0600

    loading screen done

---
+++ index.js
@@ -0,0 +1,44 @@
+let removeitem = document.getElementById('first_div_that_gets_permission');
+let removeby = document.getElementById('button_of_all_permissions')
+
+removeby.addEventListener('click', ()=>{
+    removeitem.style.opacity='0';
+    document.getElementById('div1').style.opacity='0';
+    document.getElementById('div2').style.opacity='0';
+    document.getElementById('div3').style.opacity='0';
+    document.getElementById('main_div_of_permission').style.opacity='0';
+    document.getElementById('div_of_txt_permission').style.opacity='0';
+    document.getElementById('h1as').style.opacity='0';
+    document.getElementById('button_of_all_permissions').style.opacity='0';
+    document.getElementById("logo_of_afa_from_logo_background_from_intro_background").classList.add("show");
+    document.getElementById("logo_devider_from_logo_background_from_intro_background").classList.add("show");
+    document.getElementById("FIFA_LOGO_from_logo_background_from_intro_background").classList.add("show");
+    document.getElementById("OS_logo_from_name_and_logo_div").classList.add("show");
+    document.getElementById("texts_from_name_and_logo_div").classList.add("show");
+    document.getElementById("diclaimer_txt_from_disclaimer_div_bottom").classList.add("show");
+    const item = document.getElementById("loader");
+    setTimeout(() => {
+        item.classList.add("show");
+    }, 7000);
+    setTimeout(() => {
+        item.classList.remove("show");
+        document.getElementById('intro_background').style.opacity='0';
+        document.getElementById('intro_background').style.visibility='none';
+        document.getElementById('logo_background_from_intro_background').style.opacity='0';
+        document.getElementById('logo_of_afa_from_logo_background_from_intro_background').style.opacity='0';
+        document.getElementById('logo_devider_from_logo_background_from_intro_background').style.opacity='0';
+        document.getElementById('FIFA_LOGO_from_logo_background_from_intro_background').style.opacity='0';
+        document.getElementById('loading_div').style.opacity='0';
+        document.getElementById('loading_div').style.visibility='none';
+        document.getElementById('loader').style.opacity='0';
+        document.getElementById('name_and_logo_div').style.opacity='0';
+        document.getElementById('name_and_logo_div').style.visibility='none';
+        document.getElementById('texts_from_name_and_logo_div').style.opacity='0';
+        document.getElementById('OS_logo_from_name_and_logo_div').style.opacity='0';
+        document.getElementById('disclaimer_div_bottom').style.opacity='0';
+        document.getElementById('disclaimer_div_bottom').style.visibility='none';
+        document.getElementById('diclaimer_txt_from_disclaimer_div_bottom').style.opacity='0';
+        document.body.style.backgroundColor='#ffffff'
+        //document.getElementById('').style.opacity='0';
+    }, 11000);
+});
